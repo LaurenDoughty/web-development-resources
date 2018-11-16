2c530d619e45a5418aa15c996dc41ad635ae854a
Lauren D <lefdoughty@gmail.com>
2018-11-16 08:27:49 -0500

    JS test
    test code to make sure JS is working

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+<script>
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "black";
+}
+</script>
